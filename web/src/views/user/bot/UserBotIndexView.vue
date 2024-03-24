b14be02229ae5915ed9ef228f1d3156290eb3127
xiejiamei <1976178744@qq.com>
2024-03-24 08:56:37 +0800

    5.1

--- web/src/views/user/bot/UserBotIndexView.vue
+++ web/src/views/user/bot/UserBotIndexView.vue
@@ -1,19 +1,90 @@
 <template>
-<Content>
-    我的Bot
-</Content>
+    <Content>
+        我的Bot
+    </Content>
 </template>
-    
+
 <script>
-    import Content from '../../../components/Content.vue';
-    
-    export default{
-    components:{
+import Content from '../../../components/Content.vue';
+import $ from 'jquery';
+import { useStore } from 'vuex';
+
+
+
+export default {
+    components: {
         Content,
+    },
+    setup() {
+        const store = useStore();
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/add/",
+        //     type: "post",
+        //     data: {
+        //         title: "Bot的标题",
+        //         description: "Bot的描述",
+        //         content: "Bot的代码",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/remove/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 5,
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+        // $.ajax({
+        //     url: "http://127.0.0.1:3000/user/bot/update/",
+        //     type: "post",
+        //     data: {
+        //         bot_id: 7,
+        //         title: "Bot的标题7",
+        //         description: "Bot的描述7",
+        //         content: "Bot的代码7",
+        //     },
+        //     headers: {
+        //         Authorization: "Bearer " + store.state.user.token,
+        //     },
+        //     success(resp) {
+        //         console.log(resp);
+        //     },
+        //     error(resp) {
+        //         console.log(resp);
+        //     }
+        // })
+        $.ajax({
+            url: "http://127.0.0.1:3000/user/bot/getlist/",
+            type: "get",
+            headers: {
+                Authorization: "Bearer " + store.state.user.token,
+            },
+            success(resp) {
+                console.log(resp);
+            },
+            error(resp) {
+                console.log(resp);
+            }
+        })
     }
-    }
+}
 </script>
-    
-<style scoped>
-        
-</style>
+
+<style scoped></style>
